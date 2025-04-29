0c75c0e0bf5565de49bac624a655c7fea3d228da
karolinecafisso <karolinecafisso@gmail.com>
2025-04-29 14:05:22 -0300

    Mudanças do código

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const express = require('express');
 const app = express();
 //EMITIR SINAL PRA FALAR QUE A BASE DE DADOS TÀ CONECTADA
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING)
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('Conectei a Base de Dados');
         //CONEXÃO SÓ IRÁ OCORRER QUANDO O APP EMITIR O SINAL PRONTO
         app.emit ('pronto');
     })
-    .catch (e => console.log(e));
+    .catch (e => {console.error('Erro de Conexão com o MongoDB:' , e)});
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 //  IMPORTANDO O ROUTES PRA CÁ
 const routes = require('./routes');
 // IMPORTANDO O PATH
@@ -21,35 +25,43 @@ const csrf = require ('csurf');
 
 //IMPORTANDO O MIDDELWaRE GLOBAL
 const {middlewareGlobal, meuMiddleware, checkCsrfError, csrfMiddleware} = require('./src/middleware/middleware');
-const { Console } = require('console');
-
 
 //HELMET
 app.use(helmet());
 //Para tratar POST e PUT que for enviado e não vir como undefined
-
 app.use(express.urlencoded({extended: true}));
-
 //Conteudo Estático
-
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    }
+});
+
+app.use(sessionOptions);
+app.use(flash());
 
 //Configurando os views
 
 app.set('views' , path.resolve (__dirname, 'src' , 'views'));
 app.set('view engine' , 'ejs');
 
-
 //CSURF
 app.use(csrf());
+    
 //MIDDLEWARE
-
+app.use(csrfMiddleware);
 app.use(meuMiddleware);
 app.use(middlewareGlobal);
+//MIDDLEWARE DE ERRO DEVE SER O ULTIMO
+
 app.use(checkCsrfError);
-app.use(csrfMiddleware);
-
-
 
 //DIZER PRO EXPRESS USAR AS ROTAS
 
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -15,15 +15,16 @@ exports.middlewareGlobal = (req, res, next) => {
     next();
 };
 
-
+//CHECANDO SE O TOKEN É VALIDO, SE NÃO FOR, ERRO 404
 exports.checkCsrfError = (err, req, res, next) => {
     if (err && err.code === 'EBADCSRFTOKEN') {
+        console.error('Erro de CSRF:', err);  // Log completo do erro
         return res.render('404');
-        
-    }
-    next(err); 
-};
+      }
+      next(err);  // Passa o erro adiante
+    };
 
+//CRIANDO UM MIDDLEWARE PARA DISPONIBILIZAR O TOKEN E DISPONIVEL NAS VIEWS (index.ejs)
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
